42204bce71a74cd4871062c7ae4db7f75352ecbe
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2022-08-18 23:38:02 +0000

    chore: commitlint setting

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+module.exports = { 
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "scope-enum": [
+            2, "always", [
+                "lit-practice-token",
+                "lit-practice-wc",
+                "lit-practice-react",
+                "lit-practice-vue3",
+                "lit-practice-vue27",
+                "lit-practice-vue26",
+            ],
+        ],
+        "header-max-length": [2, "always", 150],
+    }
+};
